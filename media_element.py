78da2882bb20cd005ef7f9867d475de667bb5ea5
Alok Joshi <AlokJoshi523@gmail.com>
2022-08-11 16:21:49 +0530

    Added a new timeout function

--- media_element.py
+++ media_element.py
@@ -22,8 +22,23 @@ class media_element:
         self.ws = websocket.WebSocketApp(self.kms_url,on_message= lambda ws,msg: self.on_message(ws,msg))
         self.ws_thread = threading.Thread(target = self.ws.run_forever,args=(None, None, 60, 30))
         self.ws_thread.start()
-        time.sleep(0.1)
-            
+        self.is_connected = self.ws.sock.sock
+
+        if(not self.timeout):
+            logging.error("Connection failed")
+
+    def timeout(self):
+        """ waits 0.1 seconds 10 times for successful websocket connection """
+        i = 1
+        while(i<=10):
+            if(self.is_connected):
+                return True
+            else:
+                time.sleep(0.1)
+
+            i = i+1
+        return False 
+
     def on_message(self,ws,message):
         """ Parses the message recieved from the kurento media server and handles the message by calling the appropriate callback function assigned to the server response """
 
