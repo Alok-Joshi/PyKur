ded31983130b581265da19d0f5576332c1ae170b
Alok Joshi <AlokJoshi523@gmail.com>
2022-08-25 13:30:48 +0530

    Re implemented the kurento_session using websockets, removed websocket-client dependency

--- PyKur/kurento_client.py
+++ PyKur/kurento_client.py
@@ -1,4 +1,4 @@
-import websocket
+import websockets
 import uuid
 import json
 import logging
@@ -17,13 +17,16 @@ class kurento_session:
         self.kms_url = kms_url
         self.pipeline_id = None
         self.session_id = None
-        self.ws = websocket.create_connection(kms_url)
-        self._create_media_pipeline()
-        
-    def _create_media_pipeline(self) -> None:
+        self.ws = None
+
+    async def start_session(self):
+        self.ws = await websockets.connect(self.kms_url)
+        await self._create_media_pipeline()
+
+    async def _create_media_pipeline(self) -> None:
         """Creates the media pipeline """
         params = { "type": "MediaPipeline", "constructorParams":{}, "properties": {} }
-        response = self._create(params)
+        response = await self._create(params)
 
         if "result" in response:
             self.pipeline_id  = response["result"]["value"]
@@ -32,32 +35,32 @@ class kurento_session:
         elif "error" in response:
             raise KurentoException(response["error"])
 
-    def _create(self,params):
+    async def _create(self,params):
         """ Creates the media element as mentioned in the params, and returns the response in the form of a python dictionary  """ 
 
         rpc_id = str(uuid.uuid4())
         message = generate_json_rpc(params,"create",rpc_id)
-        self.ws.send(message)
-        response = json.loads(self.ws.recv())
+        await self.ws.send(message)
+        response = json.loads(await self.ws.recv())
         return response
           
-    def close(self):
+    async def close_session(self):
         """ Closes the session by freeing the media pipeline """
         rpc_id = str(uuid.uuid4())
         params = {"object":self.pipeline_id,"sessionId":self.session_id}
         message = generate_json_rpc(params,"release",rpc_id)
 
-        self.ws.send(message)
-        response = json.loads(self.ws.recv())
+        await self.ws.send(message)
+        response = json.loads(await self.ws.recv())
         logging.info("Pipeline Released. Message from server: "+str(response))
 
-    def create_media_element(self,media_element,**kwargs):
+    async def create_media_element(self,media_element,**kwargs):
             """ Creates the media element  and  it. Argument for PlayerEndpoint: uri """
 
             kwargs.update({"mediaPipeline": self.pipeline_id})  
             params = { "type": media_element, "constructorParams": kwargs, "properties": {} ,"sessionId":self.session_id }
 
-            response = self._create(params)
+            response = await self._create(params)
 
             if "result" in response:
                 endpoint = None
